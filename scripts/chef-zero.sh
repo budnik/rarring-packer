8dbc10c1d7ecebc8f87bcc7f3f77985122a2f4ec
Dmitriy Budnik <dmbu@ciklum.com>
2014-11-05 20:34:30 +0200

    initial commit

---
+++ scripts/chef-zero.sh
@@ -0,0 +1,2 @@
+curl -L https://www.opscode.com/chef/install.sh | bash
+/opt/chef/embedded/bin/gem install nyan-cat-chef-formatter
---
+++ scripts/ruby.sh
@@ -0,0 +1,2 @@
+echo "gem: --no-ri --no-rdoc\n" > /root/.gemrc
+curl -sSL https://get.rvm.io | bash -s -- --ruby --gems bundler
